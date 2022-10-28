540be120706c3c3bfcbfc9469d4c4a6674aa9b43
kyle jenne <strongagent1@gmail.com>
2022-10-28 15:49:33 -0400

    Bank Encapsulation exercise KJ

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+
+        private double _balance = 500;
+        public double Deposit(double depAmount)
+        {
+           return _balance += depAmount;
+        }
+        public void Withdrawal(double amount)
+        {
+            _balance -= amount;
+        }
+        public double GetBalance()
+        {
+            return _balance; 
+        }
+
+        
+
+
+    }   
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace BankEncapsulation
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var custAccount = new BankAccount();
+          
+            Console.WriteLine("Please type the amount of your deposit and press enter to recieve your balance");
+
+            var depAmnt = (Console.ReadLine());
+            var depAmnt2 = Convert.ToDouble(depAmnt);
+
+            custAccount.Deposit(depAmnt2);
+
+            Console.WriteLine($"Your deposit is {depAmnt2} dollars and that " +
+                $"brings the total balance to ${custAccount.GetBalance()} ");
+           
+
         }
     }
 }
